2ade32ae974bf6f9684235f8478f7336877fb262
Paresh Kuamr <krparesh03@gmail.com>
2024-10-23 16:28:11 +0530

    first chai

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,16 @@
 
 
-
+import Chai from "./Chai"
 
 
 function App() {
  
  return(
-    <h1>first project with vite | Paresh kumar </h1>
+   <>
+    <Chai/>
+    <h1>Lorem ipsum dolor sit amet.</h1>
+    <p>text of Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla illum dignissimos sequi ipsam doloremque, aut facilis magnam vitae! Deserunt eveniet in minima culpa molestias, distinctio officiis facere. Ullam, cumque molestias.</p>
+    </>
  )
 }
 
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+     return(
+        <h3> Chai is Hot </h3>
+     )
+}
+
+export default Chai 
