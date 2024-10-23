a36b4ca45801939e6bc36d54e0769aff0e6d637c
Paresh Kuamr <krparesh03@gmail.com>
2024-10-23 18:56:33 +0530

    custom react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,11 +1,36 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App | paresh</h1>
+    </div>
+  )
+}
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
 
-createRoot(document.getElementById('root')).
+const anotherElement = (
+  <a  href= "https://google.com" target='_blank'>Visit
+  google</a>
+)
+  const reactElement = React.createElement(
+     'a',
+     {href: 'https://google.com', target: '_blank' },
+     'click me to visit google'
+  )
+
+ReactDOM.createRoot(document.getElementById('root')).
 render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+ reactElement
+
 )
