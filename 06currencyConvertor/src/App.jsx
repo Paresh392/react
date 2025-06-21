7024340f47437ec47ab7576f02af4e4f3e634dd8
Paresh Kuamr <krparesh03@gmail.com>
2025-06-21 23:22:27 +0530

    currencyConvert

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className ='text-4xl bg-purple-500' >currency App by youtube</h1>
+    
+    </>
+  )
+}
+
+export default App
